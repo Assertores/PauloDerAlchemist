3b61c7d3f2039f07fc8838ff05d7f6e6fb16a477
[HOME]Andreas <assertores@web.de>
2020-03-21 18:41:24 +0100

    [PRO] new pipe system

---
+++ PauloDerAlchemist_Unity/Assets/Code/Test2/Node.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using AsserTOOLres;
+
+namespace PDATest2 {
+	public class Node : Multiton<Node> {
+
+		public List<Pipe> m_pipes = new List<Pipe>();
+
+		public Region m_region;
+
+		Vector3 m_lastPos;
+
+		private void Start() {
+			m_lastPos = transform.position;
+		}
+
+		private void Update() {
+			if(m_lastPos != transform.position) {
+				foreach(var it in m_pipes) {
+					it.RecalculatePosition();
+				}
+			}
+		}
+
+		public bool Connect(Node other) {
+			if(other == this) {
+				return true;
+			}
+
+			if(m_region == null) {
+				m_region = other.m_region;
+				if(m_region == null) {
+					m_region = new Region();
+				}
+
+				other.m_region = m_region;
+			}else if(!m_region.Connect(other.m_region)) {
+				return false;
+			}
+
+			foreach(var it in other.m_pipes) {
+				if(it.ReplaceNode(other, this)) {
+					m_pipes.Add(it);
+				}
+			}
+
+			transform.position = other.transform.position;
+
+			Destroy(other.gameObject);
+
+			return true;
+		}
+
+		public void EnablePipeCollision(bool enable) {
+			foreach(var it in m_pipes) {
+				it.r_col.enabled = enable;
+			}
+		}
+
+		public void AddPipe(Pipe element) {
+			m_pipes.Add(element);
+		}
+
+		public void RemovePipe(Pipe element) {
+			m_pipes.Remove(element);
+
+			if(m_pipes.Count <= 0) {
+				Destroy(this.gameObject);
+			}
+		}
+	}
+}
---
+++ PauloDerAlchemist_Unity/Assets/Code/Test2/Region.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PDATest2 {
+
+	public enum element {
+		EMPTY = 0,
+		FIRE, WATER, EARTH, AIR,
+		HELIUM, OZONE, CARBON,
+		METHANOL, PROPANE, ETHAN, ACETALDEHYDE,
+		POLYVINYLCHLORIDE, POLYTETRAFLUOROETHYLENE, SLICONDIOXIDE,
+		PLASTIC, GLASS,
+		STEEL, ALUMINIUM, BRASS,
+		PLUTONIUM, RADON, URANIUM, FERMIUM,
+		SILICON, TUNGSTEN, SILVER,
+		MERCURY, PLATINUM,
+		GOLD,
+		SIZE
+	}
+
+	public class Context {
+		public element m_content;
+		public int m_amount;
+	}
+
+	public class Region {
+		Context m_context;
+
+		public element m_content { get => m_context == null ? element.EMPTY : m_context.m_content; }
+		public int m_amount { get => m_context == null ? 0 : m_context.m_amount; }
+
+		public bool Connect(Region other) {
+			Debug.Assert(other != null);
+
+			if(m_context == null) {
+				m_context = other.m_context;
+				return true;
+			}
+			if(other.m_context == null) {
+				other.m_context = m_context;
+				return true;
+			}
+
+			if(m_content != element.EMPTY && m_content != other.m_content) {
+				return false;
+			}
+
+			other.m_context.m_amount += m_context.m_amount;
+			m_context = other.m_context;
+
+			return true;
+		}
+	}
+}
