6eb0f9ace49262d38b499ceb85c3d4b57b3cd8b4
[HOME]Andreas <assertores@web.de>
2020-03-21 18:38:56 +0100

    [PRO] added some tools

---
+++ PauloDerAlchemist_Unity/Assets/Code/Tools/DDOL.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AsserTOOLres {
+	public class DDOL : MonoBehaviour {
+
+		private void Awake() {
+			DontDestroyOnLoad(this.gameObject);
+			Destroy(this);
+		}
+	}
+}
